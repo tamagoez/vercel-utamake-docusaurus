3fb3be9a49976e257db67af70fa45b6dd9cf1e31
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 17:44:08 +0900

    Update and rename sidebars.js to docs/sidebars.js

---
+++ docs/sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  someSidebar: {
+    Docusaurus: ['doc1' , 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
+  wiki: [
+    {
+      type: "doc",
+      id: "wiki/home",
+    },
+    {
+      type: "category",
+      label: "Important",
+      items: ["wiki/important/connect-note-to-site", "wiki/important/premium-user", "wiki/important/wiki-rules"],
+    },
+  ],
+};
