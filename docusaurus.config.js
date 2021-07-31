bcb49bfb483238f52800a35e51a30638b40b8dc8
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 13:06:34 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,13 +87,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tabascoes/vercel-utamake-docusaurus/edit/master/website/',
+            'https://github.com/tabascoes/vercel-utamake-docusaurus/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tabascoes/vercel-utamake-docusaurus/edit/master/website/blog/',
+            'https://github.com/tabascoes/vercel-utamake-docusaurus/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
