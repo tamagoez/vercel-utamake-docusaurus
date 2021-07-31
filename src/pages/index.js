4eb0096b21b203aa75793a0a542a840019f26622
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 15:27:33 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,32 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>美しくなったサイト</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Docusaurusは、美しさと操作性を重視して作られたサイトです。
+    　　安心安全のFaceBook社製です。
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>記事にフォーカス</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Docusaurusは記事に簡単にアクセスするのを手伝います。
+      　UtamakeNextは、コンパクトなメニューバーですぐにフォーカス。
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
+    title: <>VercelとReace</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        VercelとReactを組み合わせることによって、高速なWebエクスペリエンスを提供します。
+      　ローディングをより速く。
       </>
     ),
   },
