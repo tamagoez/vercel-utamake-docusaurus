875f13b4d483700746fbaeb51d631644d22db525
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 16:10:56 +0900

    Create sidebar.js

---
+++ wiki/sidebar.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mySidebar: [
+    {
+      type: 'category',
+      label: 'Wiki',
+      items: ['home', 'dripstone', 'exchange-gold-to-money', 'furnance-can', 'how-to-mine-ladder', 'how-to-mine-windmill', 'item-name', 'utamakeworldrecord'],
+    };
+};
