7b08f44856a5e7816606d07691b452838f7620a0
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 20:56:48 +0900

    Update sidebars.js

--- wiki/sidebars.js
+++ wiki/sidebars.js
@@ -6,5 +6,10 @@ module.exports = {
       label: 'Important',
       items: ['important/wiki-rules'],
     },
+    {
+      type: 'category',
+      label: 'Author',
+      items: ['author/tamagoes', 'author/utako', 'author/tamuke'],
+    },
   ],
 };
