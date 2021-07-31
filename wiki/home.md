d85f930dff608b4b80911edef5f5702ec1edaaa4
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 17:36:30 +0900

    Delete wiki directory

--- wiki/home.md
+++
@@ -1,7 +0,0 @@
----
-id: home
-title: Home
-sidebar_label: Home
----
-
-This is home.
--- wiki/sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mySidebar: [
-    {
-      type: 'category',
-      label: 'Wiki',
-      items: ['home', 'dripstone', 'exchange-gold-to-money', 'furnance-can', 'how-to-mine-ladder', 'how-to-mine-windmill', 'item-name', 'utamakeworldrecord']
-    };
-};
