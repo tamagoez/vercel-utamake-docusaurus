8599e91f4760d60c29474ee6da75d370b8059527
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-07-31 17:54:18 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/wiki/home')}>
               Go to NEXT
             </Link>
           </div>
