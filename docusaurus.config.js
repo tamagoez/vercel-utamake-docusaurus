852dc8b2209701a3bea566f2c200f2620071421a
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-08-01 21:04:09 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,18 +151,17 @@ module.exports = {
         showLastUpdateTime: true,
       },
     ],
-  ],
-  [
-    '@docusaurus/plugin-google-analytics',
-      {
-        themeConfig: {
-          googleAnalytics: {
-            trackingID: 'G-R6L3WQW4KB',
-            // Optional fields.
-            anonymizeIP: true, // Should IPs be anonymized?
+    [
+      '@docusaurus/plugin-google-analytics',
+       {
+          themeConfig: {
+            googleAnalytics: {
+              trackingID: 'G-R6L3WQW4KB',
+              // Optional fields.
+              anonymizeIP: true, // Should IPs be anonymized?
+            },
           },
         },
-      },
-    ],
-  ],
+      ],
+   ],
 };
